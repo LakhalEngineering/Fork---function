f341ca911bf3dd781c8940418476245fdf1f0bac
LakhalEngineering <125914042+LakhalEngineering@users.noreply.github.com>
2023-10-14 10:16:12 +0200

    Using exec function
    By using exec function we produce a process of other program in the main program

--- main.c
+++ main.c
@@ -9,6 +9,7 @@ int main(){
     if (fork()== 0){
 
         printf("I'm childish !\n");
+        execl("/bin/ls","ls","-al",NULL);
     }else
     {
         printf("I'm old and parental. \%d",getpid());
